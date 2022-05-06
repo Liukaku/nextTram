c0014255a2dae8f6a2a70f6c6288b229affea551
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-05-06 13:38:17 +0100

    added favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Results from "../components/Results";
@@ -13,6 +14,10 @@ const IndexPage = () => {
 
   return (
     <CTX.Provider value={[searchID, updateSearch]}>
+      <Head>
+        <title>Tram Stop Times</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.png" />
+      </Head>
       <div className="w-10 mx-auto bg-slate-700">
         <Search />
         <Results />
