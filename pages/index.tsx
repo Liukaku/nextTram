06035781b29ebd7fa28ce44ceff1887cd89a2bf1
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-05-08 15:48:56 +0100

    update background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,8 @@ const IndexPage = () => {
   const [searchID, updateSearch] = useState<number>();
 
   useEffect(() => {
-    document.getElementsByTagName("body")[0].style.backgroundColor = "#D1D5DB";
+    document.getElementsByTagName("body")[0].style.background =
+      "url(/background.png)";
   }, []);
 
   return (
@@ -19,7 +20,7 @@ const IndexPage = () => {
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </Head>
 
-      <div className="w-4/5 mt-20 mx-auto border-2 pb-10 border-b-gray-500 border-l-gray-500 border-r-gray-100 border-t-gray-100 shadow-xl">
+      <div className="w-4/5 mt-20 mx-auto border-2 pb-10 bg-gray-200  border-b-gray-500 border-l-gray-500 border-r-gray-100 border-t-gray-100 shadow-xl">
         <div className="p-1 text-xl text-white mb-5 headerBarGrey leading-none bg-zinc-300  border-t-zinc-200 border-r-zinc-200 border-l-zinc-400 border-b-zinc-400">
           Metrolink Tram Time Checker
         </div>
