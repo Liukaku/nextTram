0172b6d02ad8c46dd69f10dbbea014730712990c
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-05-08 15:43:15 +0100

    added header bar styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,10 @@ const IndexPage = () => {
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </Head>
 
-      <div className="w-4/5 mt-20 mx-auto border-2 border-b-gray-500 border-l-gray-500 border-r-gray-100 border-t-gray-100">
+      <div className="w-4/5 mt-20 mx-auto border-2 pb-10 border-b-gray-500 border-l-gray-500 border-r-gray-100 border-t-gray-100 shadow-xl">
+        <div className="p-1 text-xl text-white mb-5 headerBarGrey leading-none bg-zinc-300  border-t-zinc-200 border-r-zinc-200 border-l-zinc-400 border-b-zinc-400">
+          Metrolink Tram Time Checker
+        </div>
         <Search />
         <Results />
       </div>
